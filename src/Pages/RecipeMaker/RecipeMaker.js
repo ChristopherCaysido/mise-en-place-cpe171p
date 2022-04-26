00bbd085c41157c40b2a69449392fca37eda5912
ChristopherCaysido <christopher.caysido@gmail.com>
2022-04-26 21:16:40 +0800

    Added About Page, and Edited Recipe Creator Page

--- src/Pages/RecipeMaker/RecipeMaker.js
+++ src/Pages/RecipeMaker/RecipeMaker.js
@@ -1,31 +1,80 @@
 import React from "react";
 import Navbar from "../../NavBar/NavigationBar";
 import {useState} from 'react';
+import { Container, makeStyles } from "@mui/material";
+import { TextField } from "@mui/material";
+import { IconButton } from "@mui/material";
+import  Delete  from '@mui/icons-material/Delete';
+import Add from '@mui/icons-material/Add';
+import { Button } from "@mui/material"; 
+import { Send } from "@mui/icons-material";
+import { styled } from "@mui/system";
 
-export default function RecipeMaker(){
-    const [recipeList,setRecipe] = useState([{Ingredient:""}]);
-    const [ingredientList,setIngredients] = useState();
+const useStyles = makeStyles((theme)=>({
+    root: {
+        borderRadius: 12,
+        backgroundColor: 'blue';
+    },
+    button: {
+        margin:theme.spacing(1),
+    }
+}))
+
+
+
+function RecipeMaker(){
+    const classes = useStyles()
+    const [recipeList,setRecipe] = useState();
+    const [ingredientList,setIngredients] = useState([
+        {Ingredient:"",Quantity:'',Unit:''},
+    ]);
     return(
         <div>
         <Navbar/>
+        <div className='ingredients-form'>
+            <h1>Ingredients</h1>
+            <br></br>
             <form className='recipe'>
-                <div className='ingredients-form'>
-                    <label htmlFor="ingredients">Ingredient(s)</label>
-                    {ingredientList.map()}
-                    <div className='ingredients-field'>
-                        <table>
-                            <tr>
-                                <td>Name of Ingredient</td>
-                                <td>Quantity</td>
-                                <td>Unit of Measure</td>
-                            </tr>
-                            <tr>
-
-                            </tr>
-                        </table>
+                    {ingredientList.map((singleIngredient,index) =>(
+                    <div key={index} className='ingredients-field'>
+                    
+                        <TextField
+                            name='ingredientName'
+                            label='Ingredient Name'
+                            variant='filled'
+                            value={ingredientList.ingredientName}
+                        ></TextField>
+                        <TextField
+                            name='quantityName'
+                            label='Quantity'
+                            variant='filled'
+                            value={ingredientList.quantity}
+                        ></TextField>
+                        <TextField
+                            name='Unit'
+                            label='Unit'
+                            variant='filled'
+                            value={ingredientList.unitmeasure}
+                        ></TextField>
+                        <IconButton aria-label='delete'>
+                            <Delete />
+                        </IconButton>
+                        <IconButton>
+                            <Add />
+                        </IconButton>
                     </div>
+                    ))}
+                    <Button variant='contained' color='primary' type='submit'>Send<Send></Send></Button>
+                    
+                </form>
                 </div>
-            </form>
+            
+            
+
+                    
+            
         </div>
     );
 }
+
+export default RecipeMaker;
